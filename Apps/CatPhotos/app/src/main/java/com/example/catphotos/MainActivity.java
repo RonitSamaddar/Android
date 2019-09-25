b9ce54a5ba6edcee5918faf788583a1bbae7243f
Ronit Samaddar <ronitsamaddar97@gmail.com>
2019-09-26 02:12:04 +0530

    Made Udacity's Android:User Interface's HappyBirthday app and a Cat Photos app

---
+++ Apps/CatPhotos/app/src/main/java/com/example/catphotos/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.catphotos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    int c=-1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void changePhoto(View V)
+    {
+
+        System.out.println("HelloWorld");
+        ImageView I = findViewById(R.id.cat_pic);
+        int[] img={R.drawable.c1,R.drawable.c2,R.drawable.c3,R.drawable.c4,R.drawable.c5};
+        c=(c+1)%5;
+        System.out.println(img[c]);
+        I.setImageResource(img[c]);
+        //if (d != null) {
+        //    I.setImageDrawable(drawable);
+        //}
+    }
+
+}
