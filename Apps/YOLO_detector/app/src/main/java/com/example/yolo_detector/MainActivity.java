16d2d6d30223b5a29d4ff760040f41154859937d
Ronit Samaddar <ronitsamaddar97@gmail.com>
2020-07-10 00:24:25 +0530

    Trying to make a basic multi screen app initially

---
+++ Apps/YOLO_detector/app/src/main/java/com/example/yolo_detector/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.yolo_detector;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
